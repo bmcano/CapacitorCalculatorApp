d5384ec2a13dc764efcae9d2f954ccefd2f10c2f
Brandon Cano <brandoncano01@gmail.com>
2023-07-24 17:40:43 -0500

    added more content to the about page, with the cards

--- app/src/main/java/com/brandoncano/capacitorcalculatorapp/ui/screens/AboutScreen.kt
+++ app/src/main/java/com/brandoncano/capacitorcalculatorapp/ui/screens/AboutScreen.kt
@@ -2,8 +2,14 @@ package com.brandoncano.capacitorcalculatorapp.ui.screens
 
 import android.content.Context
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,7 +31,12 @@ fun AboutScreen(context: Context, navController: NavController) {
         Surface(
             modifier = Modifier.fillMaxSize(),
         ) {
-            Column {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 AboutAppBar(stringResource(R.string.about_title))
                 BottomShadow()
 
@@ -33,26 +44,49 @@ fun AboutScreen(context: Context, navController: NavController) {
                     .align(Alignment.CenterHorizontally)
                     .padding(start = 16.dp, end = 16.dp)
                 val textModifier = textModifierBody.padding(top = 16.dp)
-                TextTitle(
-                    modifier = textModifier,
-                    text = stringResource(id = R.string.app_name)
-                )
-                TextHeadline(
-                    modifier = textModifier,
-                    text = stringResource(id = R.string.about_app_version)
-                )
-                TextBody(
-                    modifier = textModifierBody,
-                    text = stringResource(id = R.string.about_version)
-                )
-                TextHeadline(
-                    modifier = textModifier,
-                    text = stringResource(id = R.string.about_created_by)
-                )
-                TextBody(
-                    modifier = textModifierBody,
-                    text = stringResource(id = R.string.about_author)
-                )
+                val cardModifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                    .fillMaxWidth()
+
+                Card(modifier = cardModifier) {
+                    TextTitle(
+                        modifier = textModifier,
+                        text = stringResource(id = R.string.app_name)
+                    )
+                    TextHeadline(
+                        modifier = textModifier,
+                        text = stringResource(id = R.string.about_app_version)
+                    )
+                    TextBody(
+                        modifier = textModifierBody,
+                        text = stringResource(id = R.string.about_version)
+                    )
+                    TextHeadline(
+                        modifier = textModifier,
+                        text = stringResource(id = R.string.about_created_by)
+                    )
+                    TextBody(
+                        modifier = textModifierBody,
+                        text = stringResource(id = R.string.about_author)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+
+                Card(modifier = cardModifier) {
+                    TextHeadline(
+                        modifier = textModifier,
+                        text = stringResource(id = R.string.about_description)
+                    )
+                    TextBody(
+                        modifier = textModifier,
+                        text = stringResource(id = R.string.about_description_part_01)
+                    )
+                    TextBody(
+                        modifier = textModifier,
+                        text = stringResource(id = R.string.about_description_part_02)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
             }
         }
     }
--- app/src/main/java/com/brandoncano/capacitorcalculatorapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/brandoncano/capacitorcalculatorapp/ui/screens/HomeScreen.kt
@@ -1,15 +1,21 @@
 package com.brandoncano.capacitorcalculatorapp.ui.screens
 
 import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FileOpen
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,11 +26,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.brandoncano.capacitorcalculatorapp.R
@@ -67,7 +76,9 @@ fun HomeScreen(context: Context, navController: NavController) {
                 HomeAppBar(stringResource(R.string.app_name), context, navController)
                 BottomShadow()
                 Card(
-                    modifier = Modifier.padding(16.dp)
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                        .fillMaxWidth()
                 ) {
                     OutlinedTextField(
                         modifier = outlinedTextFieldModifier,
@@ -148,6 +159,34 @@ fun HomeScreen(context: Context, navController: NavController) {
                         pf = capacitor.pf
                         nf = capacitor.nf
                         uf = capacitor.uf
+                    }
+                }
+                Card(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                        .fillMaxWidth()
+                        .clickable(role = Role.Button) {
+
+                        }
+                ) {
+                    Row {
+                        Image(
+                            modifier = Modifier.padding(16.dp),
+                            imageVector = Icons.Outlined.FileOpen,
+                            contentDescription = null,
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurfaceVariant)
+                        )
+                        Text(
+                            modifier = Modifier.padding(16.dp),
+                            text = "Click to view common codes",
+                            overflow = TextOverflow.Ellipsis
+                        )
+//                        Image(
+//                            modifier = Modifier.padding(16.dp),
+//                            imageVector = Icons.Filled.KeyboardArrowRight,
+//                            contentDescription = "Image",
+//                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurfaceVariant)
+//                        )
                     }
                 }
             }
