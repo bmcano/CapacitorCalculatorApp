5f6a0879c0ee05e5c1c692507d0d113867127e60
Brandon Cano <brandoncano01@gmail.com>
2023-08-07 17:21:39 -0500

    added transitions for screen navigation

--- app/src/main/java/com/brandoncano/capacitorcalculator/ui/navigation/Navigation.kt
+++ app/src/main/java/com/brandoncano/capacitorcalculator/ui/navigation/Navigation.kt
@@ -2,32 +2,44 @@ package com.brandoncano.capacitorcalculator.ui.navigation
 
 import android.content.Context
 import android.content.res.Configuration
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.brandoncano.capacitorcalculator.MainActivity
 import com.brandoncano.capacitorcalculator.ui.screens.AboutScreen
 import com.brandoncano.capacitorcalculator.ui.screens.ChartScreen
 import com.brandoncano.capacitorcalculator.ui.screens.HomeScreen
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
 /**
  * Job: Holds all the navigation information and full screen previews
  */
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Navigation(context: Context) {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.Home.route) {
+    val navController = rememberAnimatedNavController()
+    AnimatedNavHost(navController = navController, startDestination = Screen.Home.route) {
         composable(route = Screen.Home.route) {
             HomeScreen(context, navController)
         }
-        composable(route = Screen.About.route) {
+        composable(
+            route = Screen.About.route,
+            enterTransition = { slideInVertically(initialOffsetY = { it }) },
+            exitTransition = { slideOutVertically(targetOffsetY = { it }) },
+        ) {
             AboutScreen(context)
         }
-        composable(route = Screen.Chart.route) {
+        composable(
+            route = Screen.Chart.route,
+            enterTransition = { slideInVertically(initialOffsetY = { it }) },
+            exitTransition = { slideOutVertically(targetOffsetY = { it }) },
+        ) {
             ChartScreen(context)
         }
     }
