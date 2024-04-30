110140d2351a911ad4b5ea8d3ae396191a68516b
Brandon Cano <brandoncano01@gmail.com>
2024-04-29 21:49:48 -0500

    Added try catch for play store links, being consistent with both apps

--- app/src/main/java/com/brandoncano/capacitorcalculator/util/PlayStore.kt
+++ app/src/main/java/com/brandoncano/capacitorcalculator/util/PlayStore.kt
@@ -1,8 +1,9 @@
 package com.brandoncano.capacitorcalculator.util
 
+import android.app.AlertDialog
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
 
 object PlayStore {
 
@@ -10,14 +11,25 @@ object PlayStore {
     private const val RESISTOR_LINK = "https://play.google.com/store/apps/details?id=com.brandoncano.resistancecalculator"
 
     fun openCapacitorApp(context: Context) {
-        val uri = Uri.parse(CAPACITOR_LINK)
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        context.startActivity(intent)
+        open(context, CAPACITOR_LINK)
     }
 
     fun openResistorApp(context: Context) {
-        val uri = Uri.parse(RESISTOR_LINK)
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        context.startActivity(intent)
+        open(context, RESISTOR_LINK)
+    }
+
+    private fun open(context: Context, link: String) {
+        try {
+            val builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(context, Uri.parse(link))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Error")
+                .setMessage("A problem occurred while trying to open the link.")
+                .setPositiveButton("Close", null)
+                .show()
+        }
     }
 }
