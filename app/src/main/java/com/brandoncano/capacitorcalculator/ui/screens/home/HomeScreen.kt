f7329c69a60d2227cc45826d3c7d5489e923333a
Brandon Cano <brandoncano01@gmail.com>
2024-07-26 15:41:53 -0500

    1. fixed about page text. 2. added spacer to home screen

--- app/src/main/java/com/brandoncano/capacitorcalculator/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/brandoncano/capacitorcalculator/ui/screens/home/HomeScreen.kt
@@ -3,7 +3,9 @@ package com.brandoncano.capacitorcalculator.ui.screens.home
 import android.content.Context
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
@@ -12,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.brandoncano.capacitorcalculator.R
 import com.brandoncano.capacitorcalculator.ui.MainActivity
@@ -52,6 +55,7 @@ private fun ContentView(
         AppIcon()
         AppCalculatorButtons(navController)
         OurAppsButtons(context)
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
